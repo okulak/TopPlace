8f6de9616c350b9f8a0d77d8eb8f25cb70e26871
Oleksandr Kulakov <kulakov-sasha@yandex.ru>
2013-05-28 15:34:09 +0300

    Implemented showed photos from recents (Tue 3:33 PM)

--- TopPlace/RecentViewController.m
+++ TopPlace/RecentViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "RecentViewController.h"
+#import "PhotoViewController.h"
 
 @interface RecentViewController ()
 
@@ -24,22 +25,18 @@
     return self;
 }
 
-- (void)viewDidLoad
+- (void)viewWillAppear:(BOOL)animated
 {
-    [super viewDidLoad];
+    [super viewWillAppear:animated];
     NSUserDefaults *settings = [NSUserDefaults standardUserDefaults];
-    NSArray *recents = [settings objectForKey:@"photoInfo"];    
+    NSArray *recents = [settings objectForKey:@"photoInfo"];
     NSString *COUNT = @"count";
     NSSortDescriptor *countDescriptor = [[NSSortDescriptor alloc] initWithKey:COUNT ascending:NO];
     NSArray *descriptors = [NSArray arrayWithObjects:countDescriptor, nil];
     self.recents = [recents sortedArrayUsingDescriptors:descriptors];
-    [self.tableView reloadData];
-   }
-
-- (void) viewWillAppear:(BOOL)animated
-{
-   
+//    [self.tableView reloadData];
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -93,56 +90,13 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+    PhotoViewController *fvc = (PhotoViewController*)segue.destinationViewController;
+    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+    fvc.photoInformaton = [self.recents objectAtIndex:indexPath.row];
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
-}
 
 @end
